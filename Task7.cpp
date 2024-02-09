c0ff0980bb1ce25afbe38d5368520939b4ce0caf
GannaElkhen <158619932+GannaElkhen@users.noreply.github.com>
2024-02-09 21:00:04 +0200

    Add files via upload

---
+++ Task7.cpp
@@ -0,0 +1,21 @@
+#include <iostream> 
+using namespace std ;
+int main(){
+int x,y;
+cout<<"Enter the start";
+cin>>x;
+cout<<"Enter the end";
+cin>>y;
+
+while(x<=y){
+    cout<<x<<"\n";
+x++;
+}
+
+
+
+
+
+
+    return 0;
+}
---
+++ Task8.cpp
@@ -0,0 +1,20 @@
+#include <iostream> 
+using namespace std ;
+int main(){
+int n;
+char x;
+int i=1;
+cout<<"Enter the character";
+cin>>x;
+cout<<"Enter the number";
+cin>>n;
+while(i<=n){
+    cout<<x;
+    i++;
+}
+
+
+return 0;
+
+    
+}
